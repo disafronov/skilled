c8f1a800b34aaffe3bf9c1d3260d4a56366f3bcb
Dmitrii Safronov <zimniy@cyberbrain.cc>
2026-06-25 13:25:42 +0400

    feat: cron
    Signed-off-by: Dmitrii Safronov <zimniy@cyberbrain.cc>

--- apps/jobs/apps.py
+++ apps/jobs/apps.py
@@ -1,6 +1,53 @@
+import os
+
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def create_schedules(sender, **kwargs):
+    """Ensure core pipeline schedules exist in django-q2."""
+    from django_q.models import Schedule
+
+    tasks = [
+        {
+            "name": "telegram_ingest_once",
+            "func": "apps.jobs.management.commands.telegram_ingest_once.Command.handle",
+            "schedule_type": Schedule.CRON,
+            "cron": os.environ.get(
+                "Q2_TELEGRAM_INGEST_CRON",
+                os.environ.get("Q2_TELEGRAM_INGEST_INTERVAL", "* * * * *"),
+            ),
+            "repeats": -1,
+        },
+        {
+            "name": "llm_worker_once",
+            "func": "apps.jobs.management.commands.llm_worker_once.Command.handle",
+            "schedule_type": Schedule.CRON,
+            "cron": os.environ.get(
+                "Q2_LLM_WORKER_CRON",
+                os.environ.get("Q2_LLM_WORKER_INTERVAL", "* * * * *"),
+            ),
+            "repeats": -1,
+        },
+        {
+            "name": "telegram_deliver_once",
+            "func": "apps.jobs.management.commands.telegram_deliver_once.Command.handle",
+            "schedule_type": Schedule.CRON,
+            "cron": os.environ.get(
+                "Q2_TELEGRAM_DELIVER_CRON",
+                os.environ.get("Q2_TELEGRAM_DELIVER_INTERVAL", "* * * * *"),
+            ),
+            "repeats": -1,
+        },
+    ]
+
+    for task in tasks:
+        Schedule.objects.get_or_create(name=task["name"], defaults=task)
 
 
 class JobsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.jobs"
+
+    def ready(self) -> None:
+        post_migrate.connect(create_schedules, sender=self)
